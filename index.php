8fbe75bf2a0a47d91f1a0b060c72bf0bd6a0ec73
Dmitry <mail90210@mail.ru>
2022-12-15 06:55:42 +0300

    Add categories and ads

--- index.php
+++ index.php
@@ -2,6 +2,56 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Dimitry Bushuev'; // укажите здесь ваше имя
+
+$categories = [
+    "boards"        => "Доски и лыжи",
+    "attachment"    => "Доски и лыжи",
+    "boots"         => "Ботинки",
+    "clothing"      => "Одежда",
+    "tools"         =>  "Инструменты",
+    "other"         =>  "Разное",
+];
+
+$ads = [
+    [
+        "name"      => "2014 Rossignol District Snowboard",
+        "category"  => "boards",
+        "price"     => 10999,
+        "img"       => "img/lot-1.jpg",
+    ],
+    [
+        "name"      => "DC Ply Mens 2016/2017 Snowboard",
+        "category"  => "boards",
+        "price"     => 15999,
+        "img"       => "img/lot-2.jpg",
+    ],
+    [
+        "name"      => "Крепления Union Contact Pro 2015 года размер L/XL",
+        "category"  => "attachment",
+        "price"     => 8000,
+        "img"       => "img/lot-3.jpg",
+    ],
+    [
+        "name"      => "Ботинки для сноуборда DC Mutiny Charocal",
+        "category"  => "boot",
+        "price"     => 10999,
+        "img"       => "img/lot-4.jpg",
+    ],
+    [
+        "name"      => "Куртка для сноуборда DC Mutiny Charocal",
+        "category"  => "clothing",
+        "price"     => 7500,
+        "img"       => "img/lot-5.jpg",
+    ],
+    [
+        "name"      => "Маска Oakley Canopy",
+        "category"  => "other",
+        "price"     => 5400,
+        "img"       => "img/lot-6.jpg",
+    ],
+
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -57,9 +107,11 @@ $user_name = 'Dimitry Bushuev'; // укажите здесь ваше имя
                 <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
                 <ul class="promo__list">
                     <!--заполните этот список из массива категорий-->
-                    <li class="promo__item promo__item--boards">
-                        <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                    <?php foreach($categories as $category => $value): ?>
+                    <li class="promo__item promo__item--<?= $category ?>">
+                        <a class="promo__link" href="pages/all-lots.html"><?= $value ?></a>
                     </li>
+                    <?php endforeach ?>
                 </ul>
             </section>
             <section class="lots">
@@ -68,17 +120,18 @@ $user_name = 'Dimitry Bushuev'; // укажите здесь ваше имя
                 </div>
                 <ul class="lots__list">
                     <!--заполните этот список из массива с товарами-->
+                    <?php foreach($ads as $ad): ?>
                     <li class="lots__item lot">
                         <div class="lot__image">
-                            <img src="" width="350" height="260" alt="">
+                            <img src="<?= $ad['img'] ?>" width="350" height="260" alt="">
                         </div>
                         <div class="lot__info">
-                            <span class="lot__category">Название категории</span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                            <span class="lot__category"><?= $categories[$ad['category']] ?></span>
+                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $ad['name'] ?></a></h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost">цена<b class="rub">р</b></span>
+                                    <span class="lot__cost"><?= $ad['price'] ?><b class="rub">р</b></span>
                                 </div>
                                 <div class="lot__timer timer">
                                     12:23
@@ -86,6 +139,7 @@ $user_name = 'Dimitry Bushuev'; // укажите здесь ваше имя
                             </div>
                         </div>
                     </li>
+                    <?php endforeach ?>
                 </ul>
             </section>
         </main>
